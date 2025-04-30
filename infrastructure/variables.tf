91c197392a5fa0e57fd3b8fd8faae272b4da234c
Sigmala1 <105688758+Sigmala1@users.noreply.github.com>
2025-04-29 22:40:37 -0700

    Update variables.tf

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,4 +1,17 @@
-# variables.tf
-variable "region" {
-  default = "us-west-1"
+variable "aws_region" {
+  description = "AWS region to deploy to"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "bucket_name" {
+  description = "Name of the S3 bucket to create"
+  type        = string
+  default     = "cyb590-pipeline-test-bucket"
+}
+
+variable "environment" {
+  description = "Environment tag"
+  type        = string
+  default     = "dev"
 }
