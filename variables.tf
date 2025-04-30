777ad8013d8ead5f88f488d2756716e285693c84
Sigmala1 <105688758+Sigmala1@users.noreply.github.com>
2025-04-30 10:40:19 -0700

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,53 @@
+variable "aws_region" {
+  description = "The AWS region to deploy resources"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "environment" {
+  description = "The environment (dev, staging, prod)"
+  type        = string
+  default     = "development"
+}
+
+variable "project_name" {
+  description = "The name of the project"
+  type        = string
+  default     = "php-hello-world"
+}
+
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnets" {
+  description = "List of public subnet CIDR blocks"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnets" {
+  description = "List of private subnet CIDR blocks"
+  type        = list(string)
+  default     = ["10.0.3.0/24", "10.0.4.0/24"]
+}
+
+variable "container_cpu" {
+  description = "The amount of CPU to allocate to the container"
+  type        = number
+  default     = 256
+}
+
+variable "container_memory" {
+  description = "The amount of memory to allocate to the container"
+  type        = number
+  default     = 512
+}
+
+variable "app_count" {
+  description = "Number of instances of the task to run"
+  type        = number
+  default     = 2
+}
