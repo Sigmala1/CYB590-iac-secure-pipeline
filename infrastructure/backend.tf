00086046e71265e95f730c6d14fed8120bee3cc8
Aaron Siegman <siegm003@csusm.edu>
2025-04-30 13:25:34 -0700

    restructure

--- infrastructure/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "cyb590-tfstate-bucket"
-    key            = "state/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-    dynamodb_table = "cyb590-tfstate-locks"
-  }
-}
